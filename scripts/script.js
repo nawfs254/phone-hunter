7989fcb16d970f089c57a4a85021243c830b1e40
nawfs254 <19b.cse254@rimt.ac.in>
2024-03-05 20:22:23 +0600

    add modal to show phone details

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ const loadPhones = async () => {
     const phones = data.data; 
     
     displayPhones(phones)
-    console.log(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+    // console.log(`https://openapi.programming-hero.com/api/phones?search=iphone`)
 }
 
 const searchPhones = async (searchValue, isViewAll) => {
@@ -13,7 +13,7 @@ const searchPhones = async (searchValue, isViewAll) => {
     const phones = data.data; 
     displayPhones(phones, isViewAll)
 
-    console.log(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
+    // console.log(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
 }
 
 const displayPhones = (phones, isViewAll) => {
@@ -22,8 +22,8 @@ const displayPhones = (phones, isViewAll) => {
     
     phoneContainer.innerHTML = "";
 
-    console.log(phones.length)  
-    console.log('is show all', isViewAll)
+    // console.log(phones.length)  
+    // console.log('is show all', isViewAll)
 
     if(phones.length >= 9 ){
         viewAllBtn.classList.remove('hidden')
@@ -53,7 +53,7 @@ const displayPhones = (phones, isViewAll) => {
             <h1 class="text-2xl font-bold">${phones.phone_name}</h1>
             <p class="text-lg text-gray-600">${phones.slug}</p>
             <p class="text-2xl font-bold">$<span>${phones.brand}</span></p>
-            <button class="btn bg-[var(--blue)] text-white hover:text-black hover:border-[var(--blue)] text-lg md:text-xl px-5 md:px-10 py-0 md:py-1 font-bold box-content w-fit">Show Details</button>
+            <button class="btn bg-[var(--blue)] text-white hover:text-black hover:border-[var(--blue)] text-lg md:text-xl px-5 md:px-10 py-0 md:py-1 font-bold box-content w-fit" onclick="showDetails('${phones.slug}')">Show Details</button>
 
         </div>
         `;
@@ -70,7 +70,7 @@ const searchHandle = (isViewAll) => {
     const searchInput =document.getElementById('search-input');
     const searchValue = searchInput.value;
     searchPhones(searchValue, isViewAll);
-    console.log(searchValue)
+    // console.log(searchValue)
     if(searchValue === ""){
         loadPhones();
     }
@@ -93,6 +93,51 @@ const toggleLoader = (isLoading) =>{
     else{
         loader.classList.add('hidden');
     }
+}
+
+// Show details
+const showDetails = async (id) => {
+    // console.log('clicked', id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const detailsData = data.data;
+    showModal(detailsData);
+}
+
+// modal display
+const showModal = (phone) => {
+    show_details_modal.showModal();
+    const sensors = phone.mainFeatures.sensors;
+    const sensorArray = []
+    for (const sensor of sensors){
+        sensorArray.push(sensor)
+    }
+    // console.log(sensorArray)
+
+    const phoneDetailsCOntainer = document.getElementById('phone-details-container');
+    
+
+    phoneDetailsCOntainer.innerHTML = 
+    `<div class="flex justify-center">
+        <img src="${phone.image}" alt="">
+    </div>
+    <div class="details space-y-3 my-5">
+        <h3 class="text-xl md:text-2xl font-bold">${phone.name}</h3>
+        <p>Description</p>
+        <p><span class="font-bold">Storage: </span> ${phone.mainFeatures.storage} </p>
+        <p><span class="font-bold">Display Size: </span> ${phone.mainFeatures.displaySize} </p>
+        <p><span class="font-bold">Memory: </span> ${phone.mainFeatures.memory} </p>
+        <p><span class="font-bold">Sensors: </span> ${sensorArray} </p>
+        <p><span class="font-bold">Release Date: </span> ${phone?.releaseDate ? phone.releaseDate : "No information found"} </p>
+        <p><span class="font-bold">Wlan: </span> ${phone.others?.WLAN ? phone.others?.WLAN :"No information found"} </p>
+        <p><span class="font-bold">Bluetooth: </span> ${phone.others?.Bluetooth ? phone.others.Bluetooth : "No information found"} </p>
+        <p><span class="font-bold">GPS: </span> ${phone.others?.GPS ? phone.others.GPS : "No information found"} </p>
+        <p><span class="font-bold">NFC: </span> ${phone.others?.NFC ? phone.others.NFC : "No information found"} </p>
+        <p><span class="font-bold">Radio: </span> ${phone.others?.Radio ? phone.others.Radio : "No information found"} </p>
+        <p><span class="font-bold">USB: </span> ${phone.others?.USB ? phone .others.USB : "No information found"} </p>
+
+    </div>`
+
 }
 
 loadPhones();
